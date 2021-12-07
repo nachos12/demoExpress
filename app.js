828fbea8b6594f787efb14a53c71ff1355f5b6ea
laurent <lrayer12@gmail.com>
2021-12-07 15:31:57 +0100

    ajout des routes

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+//creer l app
+const app = express();
+const port = 3000;
+
+const userRoute = require('./routes/users.route');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
 
-app.post();
-app.put();
-app.delete();
-app.patch();
+//middleware
+app.use(express.json());
+
+//utilisation route
+app.use(userRoute);
 
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`
+                Example app listening at http: //localhost:${port}`)
+});
